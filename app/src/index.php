ba5e3bc87efa0b06e2710847f20869fe05d82847
0vethor0 <161180971+0vethor0@users.noreply.github.com>
2025-06-02 22:52:30 -0400

    new page

---
+++ app/src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FormularioEstudiantil</title>
+</head>
+<body>
+    <form action="" method="post">
+        <h2></h2>
+        <p></p>
+        <div class="input-wrapper">
+            <input type="text" name="nombre" id="nombre" placeholder="Nombre" required>
+        </div>
+    </form>
+</body>
+</html>
